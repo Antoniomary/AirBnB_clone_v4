ddcce5fd9329abeb35daf31c579e525838eaef12
Antoniomary <amanso.totti@yahoo.com>
2024-06-22 13:15:52 -0700

    1-hbnb.js

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   const checked = {};
-  $('INPUT[type="checkbox"]').change(function () {
+  $('input[type="checkbox"]').change(function () {
     const id = $(this).attr('data-id');
     const name = $(this).attr('data-name');
     if (this.checked) checked[id] = name;
@@ -8,7 +8,7 @@ $(document).ready(function () {
   });
 
   if (checked) {
-    names = Object.values(checked).join(', ');
-    $('DIV.amenities h4').text(names);
-  else $('DIV.amenities h4').html('&nbsp;');
+    names = Object.values(checked).sort().join(', ');
+    $('.amenities h4').text(names);
+  } else $('.amenities h4').html('&nbsp;');
 });
